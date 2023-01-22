90395063c8176ea6149c4ec19fa3911938a4e226
Machepsilon <erayokutay@hotmail.com>
2023-01-22 17:11:38 +0300

    Set User Routes

--- server/index.js
+++ server/index.js
@@ -8,7 +8,13 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 
 /* CONFIGURATIONS */
 
@@ -41,10 +47,13 @@ const upload = multer({ storage: storage });
 /* ROUTES WITH FILES */
 
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES WITHOUT FILES */
 
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP */
 
