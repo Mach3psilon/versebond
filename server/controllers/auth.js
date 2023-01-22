91b6bb4d42952d234a07cf13132ad96a05e1c1f3
Machepsilon <erayokutay@hotmail.com>
2023-01-22 16:23:11 +0300

    Authentication and authorization Add

---
+++ server/controllers/auth.js
@@ -0,0 +1,76 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/user.js";
+
+/* REGISTER USER */
+export const register = async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation,
+    } = req.body;
+
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      viewedProfile: Math.floor(Math.random() * 10000),
+      impressions: Math.floor(Math.random() * 10000),
+    });
+
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+/* LOGIN USER */
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return res.status(400).json({ message: "User does not exist" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+    res.json({
+      token,
+      user: {
+        id: user._id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        picturePath: user.picturePath,
+        friends: user.friends,
+        location: user.location,
+        occupation: user.occupation,
+        viewedProfile: user.viewedProfile,
+        impressions: user.impressions,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
--- server/index.js
+++ server/index.js
@@ -8,8 +8,10 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
 
 /* CONFIGURATIONS */
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
@@ -34,7 +36,18 @@ const storage = multer.diskStorage({
   },
 });
 
+const upload = multer({ storage: storage });
+
+/* ROUTES WITH FILES */
+
+app.post("/auth/register", upload.single("picture"), register);
+
+/* ROUTES WITHOUT FILES */
+
+app.use("/auth", authRoutes);
+
 /* MONGOOSE SETUP */
+
 const PORT = process.env.PORT || 6001;
 
 mongoose.set("strictQuery", false);
@@ -44,8 +57,6 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    app.listen(PORT, () =>
-      console.log(`MongoDB server running on port: ${PORT}`)
-    );
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
   })
   .catch((error) => console.log(`${error} did not connect`));
